ddf56bfc0c021c17553b0d40b6239ebfbf02e941
mathiastj <2278040+mathiastj@users.noreply.github.com>
2019-03-24 18:23:19 +0100

    Simple scraper endpoint added

---
+++ src/index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const cors = require('cors')
+const PORT = process.env.PORT || 5000
+const { Pool } = require('pg')
+const Scraper = require('./scraper')
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+})
+
+const app = express()
+app.use(
+  cors({
+    origin: ['https://mathiastj.github.io', 'http://localhost:3000'],
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+)
+
+app.get('/prices', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  const client = await pool.connect()
+  try {
+    const results = []
+
+    res.send(results)
+  } catch (err) {
+    console.error({ stack: err.stack, msg: err.message })
+    res.send([])
+  } finally {
+    client.release()
+  }
+})
+
+app.get('/scrape', async (req, res) => {
+  const scraper = new Scraper()
+  scraper.scrape()
+
+  res.send('OK')
+})
+
+const server = app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+})
+
+process.on('uncaughtException', err => {
+  console.warn(err)
+  server.close()
+  process.exit(1)
+})
+process.on('unhandledRejection', reason => {
+  console.warn(reason)
+  server.close()
+  process.exit(1)
+})
---
+++ src/scraper.js
@@ -0,0 +1,55 @@
+const puppeteer = require('puppeteer')
+
+const EAST_REGION = 'VestØstØst'
+const WEST_REGION = 'VestØstVest'
+
+class Scraper {
+  constructor() {
+    this.endpoint = 'https://orsted.dk/Privat/Priser/Skift-til-orsted/Koeb-el'
+    this.browserConfig = {
+      headless: true,
+      devtools: false,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'] // For running on debian in docker
+    }
+  }
+  async scrape() {
+    this.browser = await puppeteer.launch(this.browserConfig)
+    this.page = await this.browser.newPage()
+
+    this.page.goto(this.endpoint)
+
+    const eastPrice = await this.getPriceForRegion(EAST_REGION)
+    const westPrice = await this.getPriceForRegion(WEST_REGION)
+
+    console.log(eastPrice, westPrice)
+  }
+
+  async getPriceForRegion(region) {
+    const sliderContainer = await this.page.waitForXPath('//*[@id="o-geo-slide-fast-containter"]')
+    // console.log(sliderContainer)
+    const text = await this.page.evaluate(element => element.textContent, sliderContainer)
+    const noSpacedText = text.replace(/\s*/g, '')
+
+    if (noSpacedText === region) {
+      return this.getPriceWithRetry()
+    } else {
+      await sliderContainer.click()
+      return this.getPriceWithRetry()
+    }
+  }
+
+  // The price is not loaded at the same time as the page but once it's loaded both values are there
+  async getPriceWithRetry() {
+    const priceSpan = await this.page.waitForXPath('//*[@id="o-price-value-fixed"]')
+    const danishDecimalPrice = await this.page.evaluate(element => element.textContent, priceSpan)
+    const price = Number(danishDecimalPrice.replace(',', '.'))
+
+    if (isNaN(price)) {
+      return this.getPriceWithRetry()
+    } else {
+      return price
+    }
+  }
+}
+
+module.exports = Scraper
